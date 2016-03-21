a1f469055c834e5fbc4dc4e3ebd45dbb8c5a535f
Stanimir Ivanov <stan@mycompany.com>
2016-03-21 09:44:47 -0400

    Added the check-public-ip script.

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This is a repository that contains different tools I wrote through the years. Fe
 
 ## show-my-connections (Mac OS X / FreeBSD)
 This is a tool to list the current established connections your computer has to a remote host. It is useful to see if you have suspicious connections going and to investigate them further. The tool can do a WHOIS lookup on the connections.
+
+## check-public-ip (FreeBSD / FreeNAS)
+Checks the public IP of your computer/server and sends you an e-mail if it has changed. You'll need to have ssmtp installed and configured correctly. A sample ssmtp.conf file is included for Yahoo e-mails.
---
+++ check-public-ip/check-public-ip.sh
@@ -0,0 +1,59 @@
+#!/usr/local/bin/bash
+
+# This is a FreeBSD script to check if your IP has changed and send you an e-mail if so.
+# It needs ssmtp to be configured correctly to send the mail.
+
+function getIp {
+	dig +short myip.opendns.com @resolver1.opendns.com
+}
+
+function sendMail {
+	message="$1"
+	printf "Sending the following message:\nFrom: pradqdo@yahoo.com\nTo: pradqdo@yahoo.com\nSubject: IP Address Change\n\n${message}"
+	printf "From: pradqdo@yahoo.com\nTo: pradqdo@yahoo.com\nSubject: IP Address Change\n\n$message" | /usr/local/sbin/ssmtp pradqdo@yahoo.com
+}
+
+# The file that stores our last valid public IP address
+ipAddressFile="/root/currentIp.txt"
+
+date
+
+publicIp=$( getIp )
+
+echo "The public IP: ${publicIp}"
+
+if [ -z "$publicIp" ]
+then
+	echo "Can't retrieve the public IP."
+	exit 1
+fi
+
+# If the file doesn't exist then create it
+if [ ! -f "$ipAddressFile" ]
+then
+	echo "Creating the IP storage file $ipAddressFile"
+	echo $publicIp > $ipAddressFile
+	exit 0
+fi
+
+# Read the currently saved IP
+currentIp=$( cat $ipAddressFile )
+
+echo "The current IP: ${currentIp}"
+
+# If the current IP is NULL for some reason
+if [ -z "$currentIp" ]
+then
+	echo $publicIp > $ipAddressFile
+	exit 0
+fi
+
+if [ "$publicIp" != "$currentIp" ]
+then
+	# Send e-mail
+	message="The IP has changed from $currentIp to $publicIp. Sending mail."
+	echo $message
+	sendMail "$message"
+	echo $publicIp > $ipAddressFile
+fi
+
