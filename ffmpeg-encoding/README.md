6bea9864f55f7cac4d5b4f2955193617ff0a6af7
Stanimir Ivanov <stan@mycompany.com>
2020-08-26 16:45:51 -0400

    Added resolution setting.

--- ffmpeg-encoding/README.md
+++ ffmpeg-encoding/README.md
@@ -5,6 +5,6 @@ Various tools to help with ffmpeg encoding
 Helps encode screen recordings from quick time on Mac OS X. It copies the audio directly but compresses the video using libx264 codec so it can be freely accesible on different operating systems.
 
 ```
-Usage: ./encode-training.sh inputFile outputFile
+Usage: ./encode-training.sh inputFile outputFile resolution(1080p or 720p)
 ```
-Just specify the input file and the output file and the conversion should start. Look inside the script if you'd like to modify any of the parameters.
+Just specify the input file and the output file and the conversion should start. Resolution is optional and defaults to 1080p. Look inside the script if you'd like to modify any of the parameters.
--- ffmpeg-encoding/encode-training.sh
+++ ffmpeg-encoding/encode-training.sh
@@ -3,23 +3,40 @@
 # This is a helper script to encode a video training such as a screen recording.
 # It should produce a well compressed video size using the commong libx264 library.
 # The video quality should be very good.
-# Audio is copied directly.
+# # Audio is copied directly.
 
 inputFile=$1
 outputFile=$2
+resolutionString=$3
 
-usageString="Usage: $0 inputFile outputFile"
+usageString="Usage: $0 inputFile outputFile resolution(1080p or 720p)"
 
-if [ -z $inputFile ]; then
+if [ -z "$inputFile" ]; then
 	echo "No input file."
 	echo $usageString
 	exit 1
 fi
 
-if [ -z $outputFile ]; then
+if [ -z "$outputFile" ]; then
 	echo "No output file."
 	echo $usageString
 	exit 1
+fi
+
+if [ -z "$resolutionString" ]; then
+	resolution="1080p"
+fi
+
+if [ "${resolutionString}" != "1080p" ] && [ "${resolutionString}" != "720p" ]; then
+	echo "Bad resolution."
+	echo $usageString
+	exit 1
+fi
+
+# Default to 720p
+width=1280
+if [ "${resolutionString}" == "1080p" ]; then
+	width=1920
 fi
 
 # Encode an ffmpeg video
@@ -27,4 +44,4 @@ fi
 # -preset controls compression. The faster the less compression applied. Allowed values: ultrafast, superfast, faster, fast, medium, slow, slower, veryslow etc.
 # Audio is aac with 128k bitrate
 # Video is scaled to 1080p with 1920 width and calculated height based on the original resolution
-ffmpeg -i "$inputFile" -vcodec libx264 -c:a aac -b:a 128k -vf "scale=1920:-2" -crf 23 -preset veryslow "$outputFile"
+ffmpeg -i "$inputFile" -vcodec libx264 -c:a aac -b:a 128k -vf "scale=${width}:-2" -crf 23 -preset medium "$outputFile"
