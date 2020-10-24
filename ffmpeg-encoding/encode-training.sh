9ad61aeadc67fa37c3f61543c8c7c94dc82af5d0
Stanimir Ivanov <stan@mycompany.com>
2020-10-24 15:33:11 -0400

    Added more parameters to improve the encoding speed and quality

--- ffmpeg-encoding/encode-training.sh
+++ ffmpeg-encoding/encode-training.sh
@@ -44,23 +44,25 @@ fi
 
 # Encode an ffmpeg video
 # -vcodec uses x264 codec. For reference of all the options check: https://trac.ffmpeg.org/wiki/Encode/H.264
-# Audio is aac with 128k bitrate
-# scale is controlled by the width with automatic height calculation
-# -crf controls the quality. Lower values = better quality. 23-28 should be good compromise.
+# -crf controls the quality. Lower values = better quality. 23-28 should be good compromise. I found that 32 is good enough.
 #    The range of the CRF scale is 0–51, where 0 is lossless, 23 is the default, and 51 is worst quality possible.
 #    A lower value generally leads to higher quality, and a subjectively sane range is 17–28. Consider 17 or 18 to be
 #    visually lossless or nearly so; it should look the same or nearly the same as the input but it isn't technically lossless.
 #    Choose the highest CRF value that still provides an acceptable quality. If the output looks good, then try a higher value.
 #    If it looks bad, choose a lower value.
+# scale is controlled by the width with automatic height calculation
+# use 24 fps. Normally a screen recording might use ~ 60 fps which is too much. This will improve the encoding speed
+# Audio is aac with 128k bitrate. Quicktime on Mac OS X doesn't like mp3 even though it results in a better file size.
 # -preset controls compression. The faster the less compression applied. Allowed values: ultrafast, superfast, faster, fast, medium, slow, slower, veryslow etc.
 #    Go with the slowest preset you can tolerate. -veryslow is the slowest usable that gives the best quality for compression.
+# -profile controls x264 profile settings. main is used for maximum compatibility but high provides better encoding speeds and output file size.
 # -tune controls extra tunning parameters. Valid values are: film, animation, grain, stillimage, fastdecode, zerolatency
 
 # File Size Considerations
 #    Go with higher crf settings and lower resolution (width) in order to decrease the size.
 #    Currently the size is optimized to be less than 350MB for 1h 30min video.
 
-command="ffmpeg -i '$inputFile' -vcodec libx264 -c:a aac -b:a 128k -vf 'scale=${width}:-2' -crf 30 -preset veryslow -tune stillimage '$outputFile'"
+command="ffmpeg -i '$inputFile' -vcodec "libx264" -crf 32 -vf 'scale=${width}:-2,fps=24' -c:a aac -b:a 128k -preset veryslow -profile:v high -tune stillimage -f mp4 '$outputFile'"
 
 echo "Running ffmpeg encoding:"
 echo $command
