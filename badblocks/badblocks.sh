d154dfa4ef409f54fbf7aa3a3eb3e0cfa9bfa1ff
Stanimir Ivanov <stan@mycompany.com>
2021-07-05 22:47:54 -0400

    Fixed the cluster size

--- badblocks/badblocks.sh
+++ badblocks/badblocks.sh
@@ -6,8 +6,8 @@ badblocks_path=/usr/local/Cellar/e2fsprogs/1.46.2_1/sbin/badblocks
 # -v verbos
 # -s progress stats. Displays a rough estimate of the durationa and percent completed.
 # -b blocksize - normally 512 bytes
-# -c cluster size? how many blocks to read. This improves speed significantly.
+# -c cluster size? how many blocks to read. This improves speed significantly. The higher the number the faster the test but also the more RAM it uses.
 # Use both -b and -c to have the optimum speed. Otherwise the test takes too long.
 # You can specify an offset at the end if want to continue a previous test.
 # The format is end_offset start_offset after the /dev/disk2
-sudo $badblocks_path -svn -b 512 -c 65536 /dev/disk2
+sudo $badblocks_path -svn -b 512 -c 200000 /dev/disk2
