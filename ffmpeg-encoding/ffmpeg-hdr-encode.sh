ccbdd9290ff4f203a35da20ce0bf43ef05882dd4
Stanimir Ivanov <stan@mycompany.com>
2022-03-11 09:37:45 -0500

    Improved the conversion scripts

--- ffmpeg-encoding/ffmpeg-hdr-encode.sh
+++ ffmpeg-encoding/ffmpeg-hdr-encode.sh
@@ -11,16 +11,19 @@ movieName=$(basename "$source" .mkv)
 echo "Converting $movieName"
 echo "Mode $conversion"
 
+hdrParams=$(./list-hdr-info-from-source-video.sh $source)
+
+echo "Using HDR params: $hdrParams"
+
 options=""
 if [ "$conversion" == "1" ]; then
-	# hdr-opt=1 we are telling it yes, we will be using HDR
-    # repeat-headers=1we want these headers on every frame as required
-    # colorprim, transfer and colormatrix the same as ffprobe listed
-    # master-display this is where we add our color string from above
-    # max-cll Content light level data, in our case 0,0
-	options="-c:v libx265 -crf 28 -preset superfast -pix_fmt yuv420p10le -x265-params keyint=60:bframes=3:vbv-bufsize=75000:vbv-maxrate=75000:hdr-opt=1:repeat-headers=1:colorprim=bt2020:transfer=smpte2084:colormatrix=bt2020nc:master-display=G(13250,34500)B(7500,3000)R(34000,16000)WP(15635,16450)L(40000000,50):max-cll=999,100:profile=main10"
+	# A movie is X frames per second. For example 23 frames per second. The encoding time depends on how many frames a second we can get from our PC.
+	# Slow is very slow: 3.1 frames a second meaning it will take 7.5 seconds to encode 1 movie second since 7.5 * 3.1 = 24 frames which is a movie second (if the movie is 23 frames per second).
+	# So the actual encoding time would be 7.5 time the number of movie seconds. For 1 hours movie this is 7.5 hours to finish the encoding.
+	# Faster is better: 11 frames a second meaning 2.1 seconds to encode a movie second. This will take 2.1 hours to finish encoding a 1 hour movie. A little bit better.
+	options="-c:v libx265 -crf 21 -preset veryfast -profile:v main10 $hdrParams"
 elif [ "$conversion" == "2" ]; then
-	options="-c:v hevc_videotoolbox -profile:v main10 -tag:v hvc1 -pix_fmt yuv420p10le "
+	options="-c:v hevc_videotoolbox -profile:v main10 -tag:v hvc1 -pix_fmt:v yuv420p10le -b:v 8000k -color_primaries:v bt2020 -color_trc:v smpte2084 -colorspace:v bt2020nc"
 elif [ "$conversion" == "3" ]; then
 	options="-c:v copy -pix_fmt yuv420p10le "
 else
@@ -40,10 +43,9 @@ fi
 # The map tells ffmpeg which stream to process the first number is 0 for copy, the second is the stream ID. You can see it with ffmpeg -i.
 #-map 0:3 -c:s copy \
 #-map 0:2 -c:a aac -ac 2 \
+#~/code/ffmpeg/ffmpeg -hide_banner \
 /usr/local/opt/ffmpeg/bin/ffmpeg -hide_banner \
 	-i "$source" \
-	-analyzeduration 20000000 -probesize 20000000 \
-	-ss 00:00:00 -t 00:01:00 \
 	-map_chapters 0 \
 	-metadata title="$movieName" \
 	-map 0:0 \
--- ffmpeg-encoding/list-hdr-info-from-source-video.sh
+++ ffmpeg-encoding/list-hdr-info-from-source-video.sh
@@ -4,7 +4,7 @@
 
 json=$(ffprobe -hide_banner -loglevel warning -select_streams v -print_format json -show_frames -read_intervals "%+#1" -show_entries "frame=color_space,color_primaries,color_transfer,side_data_list,pix_fmt" -i "$1" | jq .frames)
 
-echo $json | jq
+echo $json | >&2 jq
 
 pix_fmt=$(echo $json | jq  -r '.[].pix_fmt')
 color_space=$(echo $json | jq  -r '.[].color_space')
@@ -24,26 +24,27 @@ max_luminance=$(echo $json | jq  '.[].side_data_list' | jq -r '.[0].max_luminanc
 max_content=$(echo $json | jq  '.[].side_data_list' | jq -r '.[1].max_content' | cut -d'/' -f 1)
 max_average=$(echo $json | jq  '.[].side_data_list' | jq -r '.[1].max_average' | cut -d'/' -f 1)
 
-echo "pix_fmt: $pix_fmt"
-echo "color_space: $color_space"
-echo "color_primaries: $color_primaries"
-echo "color_transfer: $color_transfer"
-echo "red_x $red_x"
-echo "red_y $red_y"
-echo "green_x $green_x"
-echo "green_y $green_y"
-echo "blue_x $blue_x"
-echo "blue_y $blue_y"
-echo "min_luminance $min_luminance"
-echo "max_luminance $max_luminance"
-echo "max_content $max_content"
-echo "max_average $max_average"
-echo ""
-echo "Use these x265 params:"
+>&2 echo "pix_fmt: $pix_fmt"
+>&2 echo "color_space: $color_space"
+>&2 echo "color_primaries: $color_primaries"
+>&2 echo "color_transfer: $color_transfer"
+>&2 echo "red_x $red_x"
+>&2 echo "red_y $red_y"
+>&2 echo "green_x $green_x"
+>&2 echo "green_y $green_y"
+>&2 echo "blue_x $blue_x"
+>&2 echo "blue_y $blue_y"
+>&2 echo "min_luminance $min_luminance"
+>&2 echo "max_luminance $max_luminance"
+>&2 echo "max_content $max_content"
+>&2 echo "max_average $max_average"
+>&2 echo ""
+>&2 echo "Use these x265 params:"
 
 # hdr-opt=1 we are telling it yes, we will be using HDR
 # repeat-headers=1we want these headers on every frame as required
 # colorprim, transfer and colormatrix the same as ffprobe listed
 # master-display this is where we add our color string from above
 # max-cll Content light level data, in our case 0,0 (max_content,max_average)
-echo "-pix_fmt $pix_fmt -x265-params hdr-opt=1:repeat-headers=1:colorprim=$color_primaries:transfer=$color_transfer:colormatrix=$color_space:master-display=G($green_x,$green_y)B($blue_x,$blue_y)R($red_x,$red_y)WP($white_point_x,$white_point_y)L($max_luminance,$min_luminance):max-cll=$max_content,$max_average:profile=main10"
+# asm=avx512 can lead to better performance on gen 10+ processors
+echo "-pix_fmt $pix_fmt -x265-params hdr-opt=1:repeat-headers=1:colorprim=$color_primaries:transfer=$color_transfer:colormatrix=$color_space:master-display=G($green_x,$green_y)B($blue_x,$blue_y)R($red_x,$red_y)WP($white_point_x,$white_point_y)L($max_luminance,$min_luminance):max-cll=$max_content,$max_average:asm=avx512"
